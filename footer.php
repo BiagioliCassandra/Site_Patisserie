894ab9270217898d66fd853bea9c618d1af2aa72
cassi <biagioli.cassandra@gmail.com>
2018-10-12 16:34:04 +0200

    add css header, html footer

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!--~~~~~~~~~~~~~~~~~~~~
+      Start footer
+~~~~~~~~~~~~~~~~~~~~~-->
+<footer>
+    <div class="desktop">
+      <a href="https://www.iletaitunefoislapatisserie.com/">Il était une fois</a>
+      <a href="https://cook-shop.fr/fr/">Le cook-shop</a>
+      <a href="http://www.lenotre.com/e-boutique/carte-sucree/patisseries.html">Lenotre</a>
+      <a href="http://www.gourmand-croquant.com/">Gourmand-croquand</a>
+      <a href="http://chezbogato.fr/">Chezbogato</a>
+      <a href="http://piroulie.canalblog.com/">Piroulie</a>
+      <a href="https://arnaudlarher.com/">Arnaudlarher</a>
+      <a href="http://christophemichalak.com/">Christophemichalak</a>
+      <a href="https://www.planete-gateau.com/">Planete-gateau</a>
+    </div>
+    <p>Crédit</p>
+    <div>
+      <a href="https://twitter.com/"><img src="img/icons8_twitter.png" alt="Le logo de twitter"></a>
+      <a href="https://fr.linkedin.com/"><img src="img/icons8_linkedin.png" alt="Le logo de Linkedin"></a>
+      <a href="https://github.com/BiagioliCassandra/Patisserie/tree/master"><img src="img/icons8_github.png" alt="Le logo de GitHub"></a>
+      <a href="https://stackoverflow.com/"><img src="img/icons8_pile.png" alt="Le logo de Stack"></a>
+    </div>
+  </footer>
+<!--~~~~~~~~~~~~~~~~~~~~
+      End header
+~~~~~~~~~~~~~~~~~~~~~-->
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Les mignardises d'Emmanuel</title>
+  <meta name="description" content="Le site de pâtisserie d'Emmanuel">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Pacifico" rel="stylesheet"> 
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<!--~~~~~~~~~~~~~~~~~~~~
+      Start header
+~~~~~~~~~~~~~~~~~~~~~-->
+  <header>
+  <!--The section containing logo, login and search-->
+    <div class="flex icon_header">
+      <div class="margin_2em">
+        <i><img src="img/icons8_confiserie_50.png" alt="Une image de cupcake"></i>
+      </div class="margin_2em">
+      <div class="flex search margin_2em">
+        <i><img src="img/icons8_utilisateur_neutre_50.png" alt="Une icone de login"></i>
+        <form class="desktop tablette">
+          <input type="search" id="search" name="search" placeholder="Chercher sur le site">
+        </form>
+        <i class="tablette"><img src="img/icons8_chercher_26.png" alt="Une icone de recherche"></i>
+      </div>
+    </div>
+  <!--Nav containing the link for the sections of the page-->
+    <nav class="desktop margin_2em tablette">
+      <button><a class="margin_auto" href="#callToAction">Vos avis</a></button>
+      <button><a class="margin_auto" href="#presentation">Ma présentation</a></button>
+      <button><a class="margin_auto" href="#news">Les dernières nouveautés</a></button>
+      <button><a class="margin_auto" href="#recipes">Toutes les recettes</a></button>
+    </nav>
+  <!--Section contaigning presentation user-->
+    <section class="center margin_2em">
+        <h1>Les magnardises d'Emannuel</h1>
+        <p class="p_header">Bienvenue sur mon site de pâtisserie, je présenterais ici toutes mes recettes et mes dernières créations.</p>
+      <!--Background image here-->
+    </section>
+  </header>
+<!--~~~~~~~~~~~~~~~~~~~~
+      End header
+~~~~~~~~~~~~~~~~~~~~~-->
